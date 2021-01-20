8a5ececdcd11c7f1e5208992505b210e17036d03
Laxman Koirala <nirlax10000BC>
2021-01-20 02:14:37 +0200

    removing hardcoded and added one extra file

--- SystemTest/SystemTestRun.py
+++ SystemTest/SystemTestRun.py
@@ -1,50 +1,36 @@
-import openpyxl
+from SystemTest import shortcut as sc
 import pathlib
-#theFile = openpyxl.load_workbook('SystemTestCases.xlsx')
-theFile = openpyxl.load_workbook('SystemTestExecutionResultV1.48.8.xlsx')
+import os
 
-allSheetNames = theFile.sheetnames
-#path = 'D:\svn\DanpheEMR\SystemTest'
+def files(file,file1,rows,*args):
+    if not args:
+        sc.copyoriginal(file,file1)
+    for r in range(2, rows + 1):
+        Testcase = str(sc.readData(file1, 'Sheet1', r, 1))
+        Status = sc.readData(file1, 'Sheet1', r, 2)
+        RunNoR = (sc.readData(file1, 'Sheet1', r, 4))
+        if Status != 'Passed':
+            base_dir = str(pathlib.PureWindowsPath(r'D:\svn\DanpheEMR\SystemTest'))
+            Pythonfilepath = os.path.join(base_dir, Testcase)
+            print(Pythonfilepath)
 
-print("All sheet names {} " .format(theFile.sheetnames))
 
-def find_specific_cell():
-    for row in range(2, 59):
-        for column in "B":  # Here you can add or reduce the columns
-            cell_name = "{}{}".format(column, row)
-            cellstatus = "{}{}".format("D", row)
-            #tcStatus = currentSheet[cell_name].value
-            if  currentSheet[cell_name].value != "Passed":
-                print(" I am executing no run & failed test cases")
-                #cell_namer = "{}{}".format('A', row)
-                cell_namer = "{}{}".format('A', row)
-                print("Test Case Executing", currentSheet[cell_namer].value)
-                tctorun = currentSheet[cell_namer].value
-                base_dir = pathlib.PureWindowsPath(r'D:\svn\DanpheEMR\SystemTest')
-                filenametorun = base_dir / tctorun
-                print(filenametorun)
-                try:
-                    exec(open(filenametorun).read())
-                    currentSheet[cell_name].value = "Passed"
-                except Exception:
-                    currentSheet[cell_name].value = "Failed"
-                    currentSheet[cellstatus].value = currentSheet[cellstatus].value + 1
-                    pass
-                theFile.save('SystemTestExecutionResultV1.48.8.xlsx')
-                print("Updating status")
-                print(currentSheet)
-                print(cell_name)
-                print(currentSheet[cell_name].value)
-                #return cell_name
+            try:
+                exec(open(Pythonfilepath).read())
+                sc.writeData(file1, 'Sheet1', r, 2, 'Passed')
+            except:
+                sc.writeData(file1, 'Sheet1', r, 2, 'Failed')
+                sc.writeData(file1, 'Sheet1', r, 4, RunNoR + 1)
 
-def get_all_values_by_cell_letter(letter):
-    for row in range(1, currentSheet.max_row + 1):
-        for column in letter:
-            cell_name = "{}{}".format(column, row)
-            #print(cell_name)
-            print("cell position {} has value {}".format(cell_name, currentSheet[cell_name].value))
 
-for sheet in allSheetNames:
-    print("Current sheet name is {}" .format(sheet))
-    currentSheet = theFile[sheet]
-    specificCellLetter = (find_specific_cell())
+
+
+Originalfile= "SystemTestCases.xlsx"
+duplicatefile = "SystemTestExecutionResultV1.48.8.xlsx"
+rows = sc.getTotalrows(Originalfile, 'Sheet1')
+files(Originalfile,duplicatefile,rows)
+files(Originalfile,duplicatefile,rows,2) #if you don't want to do test twice then you should comment this
+
+
+
+
