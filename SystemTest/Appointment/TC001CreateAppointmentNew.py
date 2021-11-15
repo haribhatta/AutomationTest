6697e803b30c3bc25cb2d47b307bb01a37914dcf
haribhatta <harishbnv@gmail.com>
2021-11-15 15:53:03 +0545

    Added LocalShare variable file

--- SystemTest/Appointment/TC001CreateAppointmentNew.py
+++ SystemTest/Appointment/TC001CreateAppointmentNew.py
@@ -1,23 +1,21 @@
-from TestActionLibrary import A
-from GlobalShareVariables import GSV
+import AutomationTest.Library.ApplicationConfiguration as AC
+import AutomationTest.Library.GlobalShareVariables as GSV
+import AutomationTest.Library.LibModuleBilling as LB
+import AutomationTest.Library.LibModuleAppointment as LA
 
+AC.applicationSelection()
+AC.openBrowser()
+#############
 # front desk user login
 foUserId = GSV.foUserID
 foUserPwd = GSV.foUserPwD
-
-a = A()
-
-a.openBrowser()
-a.login(foUserId, foUserPwd)
-a.counteractivation()
-a.patientquickentry(discountpc=0, paymentmode='Cash')
+departmentGynae = GSV.departmentGyno
+doctorGynae = GSV.doctorGyno
+#############
+AC.login(foUserId, foUserPwd)
+LB.counteractivation()
+LA.patientquickentry(discountpc=0, paymentmode='Cash', department=departmentGynae, doctor=doctorGynae)
 #can.verifyopdinvoice(deposit=0, billamt=500)
-a.logout()
-a.closeBrowser()
+AC.logout()
+AC.closeBrowser()
 print("Status:Passed - > TC001 CreateAppointmentNew")
-
-a.openBrowser()
-a.login(foUserId, foUserPwd)
-a.counteractivation()
-a.patientRegistration()
-a.logout()
