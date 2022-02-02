e3ea2e38997d079f7a84075847f8c6e9ee7708cc
DELL <puskarkhadka12@gmail.com>
2022-02-02 11:02:20 +0545

    change in TC002InpatientMorbidityReport.py

--- SystemTest/MedicalRecords/Reports/TC002InpatientMorbidityReport.py
+++ SystemTest/MedicalRecords/Reports/TC002InpatientMorbidityReport.py
@@ -1,25 +1,30 @@
 import Library.GlobalShareVariables as GSV
 import Library.ApplicationConfiguration as AC
-import Library.LibModuleBilling as LB
-import Library.LibModuleBillingReports as LBR
-import Library.LibModuleAppointment as LA
-import Library.LibModulePatientPortal as LP
-import Library.LibModuleADT as LADT
 import Library.LibModuleMedicalRecords as LMR
 import Library.LibModuleMedicalRecordsReports as LMRR
+import Library.LibModuleAppointment as LA
+import Library.LibModuleBilling as LB
+import Library.LibModuleADT as ADT
 import time
 
 # front desk user login
-mrUserId = GSV.MRUserID
+mrUserId = GSV.adminUserID
 mrUserPwd = GSV.MRUserPwD
 opdAmt = GSV.opdRate
 user = GSV.foUserID
+departmentGynae = GSV.departmentGyno
+doctorGynae = GSV.doctorGyno
+priceCategoryType = "Normal"
 
 EMR = AC.openBrowser()
 AC.login(mrUserId, mrUserPwd)
 LMRR.getInpatientMorbidityReport(EMR)
 LMRR.storeInpatientMorbidityReport(EMR)
-LMR.addMRwithDischargeTypeDeath(EMR)
+LB.counteractivation(EMR)
+HospitalNo = LA.patientquickentry(danpheEMR=EMR, discountScheme=0, paymentmode='Cash', department=departmentGynae, doctor=doctorGynae, priceCategoryType=priceCategoryType).HospitalNo
+ADT.admitDisTrans(EMR, 1, 0, 0, HospitalNo, 0, doctorGynae, departmentGynae)
+ADT.admitDisTrans(EMR, 0, 1, 0, HospitalNo, 0, doctorGynae, departmentGynae)
+LMR.addMRwithDischargeTypeDeath(EMR, HospitalNo=HospitalNo)
 LMRR.getInpatientMorbidityReport(EMR)
 LMRR.verifyInpatientMorbidityReport(EMR)
 
