16d36537bb5a38484132e71d92706f7b8ed1e5f5
haribhatta <harishbnv@gmail.com>
2022-02-07 15:35:45 +0545

    added dailycollection vs handover comparision report

--- SystemTest/Billing/Handover/TC01DailyCollectionVsHandoverReport.py
+++ SystemTest/Billing/Handover/TC01DailyCollectionVsHandoverReport.py
@@ -1,3 +1,8 @@
+'''
+Objective:
+1. To Verify 'Total Collection' amount in 'Daily Collection Vs Handover Report' is equal to 'User Collection Report'.
+'''
+
 import Library.GlobalShareVariables as GSV
 import Library.ApplicationConfiguration as AC
 import Library.LibModuleBilling as LB
@@ -19,13 +24,8 @@ discountScheme = GSV.discountSchemeName
 EMR = AC.openBrowser()
 AC.login(itUserId, itUserPwd)
 LB.counteractivation(EMR)
-time.sleep(2)
-InvoiceNo = LA.patientquickentry(danpheEMR=EMR, discountScheme=0, paymentmode='Cash', department=GSV.departmentGyno, doctor=GSV.doctorGyno, priceCategoryType=priceCategoryType).InvoiceNo
-LBR.getTotalItemsBill(EMR)
-LBR.preSystemTotalItemsBill()
-LB.returnBillingInvoice(EMR, InvoiceNo, "This is cash return.")
-LBR.getTotalItemsBill(EMR)
-LBR.verifyTotalItemsBill(returnamt=opdAmt)
+LBR.verifyUserCollectionVsHandOverReport(EMR)
+#LBR.verifyTotalItemsBill(returnamt=opdAmt)
 time.sleep(2)
 AC.logout()
 AC.closeBrowser()
