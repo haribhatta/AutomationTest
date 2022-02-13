9a28eb47f918c021af056bdc21e28511e23cc2b4
haribhatta <harishbnv@gmail.com>
2022-02-13 17:43:05 +0545

    moved inventory reports inside respective folders.

--- SystemTest/Inventory/Reports/Stock/TC003 InventorySummaryReport.py
+++ SystemTest/Inventory/Reports/Stock/TC003 InventorySummaryReport.py
@@ -6,14 +6,16 @@ import Library.LibModuleInventory as LI
 import Library.LibModuleSubStore as LSS
 
 # front desk user login
-storeUserId = GSV.storeUserID
-storeUserPwd = GSV.storeUserPwD
+storeUserId = GSV.adminUserID
+storeUserPwd = GSV.adminUserPwD
 
 item = "Pencil"
 rate = 5
 qty = 1
-store1 = "Main Store"
-store2 = "Accounting Store"
+costAmount = rate * qty
+
+subStore1 = GSV.SubStore1
+subStore2 = GSV.SubStore2
 ########
 EMR = AC.openBrowser()
 AC.login(storeUserId, storeUserPwd)
@@ -22,11 +24,11 @@ LI.getInventorySummaryReport(EMR)
 LI.preInventorySummaryReport()
 LI.createInventoryGoodReceipt(EMR, qty, item, rate)
 LI.receiveGoodReceipt(EMR)
-RequsitionNo = LI.createInventoryDirectDispatch(EMR, item, qty, "General Inventory", store2)
+RequsitionNo = LI.createInventoryDirectDispatch(danpheEMR=EMR, itemname=item, qty=qty, inventory="General Inventory", store=subStore1)
 print(RequsitionNo)
-LSS.receiveInventoryDispatch(EMR, substore=store2, ssReqNo=RequsitionNo)
-LSS.createNewConsumption(danpheEMR=EMR, substore=store2, itemName=item)
+LSS.receiveInventoryDispatch(EMR, substore=subStore1, ssReqNo=RequsitionNo)
+LSS.createNewConsumption(danpheEMR=EMR, substore=subStore1, itemName=item)
 LI.getInventorySummaryReport(EMR)
-LI.verifyInventorySummaryReport(purchaseqty=1, purchaseamount=5, consumeqty=1, consumeamount=8, manageinqty=0, manageinamount=0, manageoutqty=0, manageoutamount=0)
+LI.verifyInventorySummaryReport(purchaseqty=1, purchaseamount=costAmount, consumeqty=1, consumeamount=costAmount, manageinqty=0, manageinamount=0, manageoutqty=0, manageoutamount=0)
 AC.logout()
 AC.closeBrowser()
