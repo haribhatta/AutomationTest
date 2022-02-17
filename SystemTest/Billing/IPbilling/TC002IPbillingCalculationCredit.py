cc410053473964b509f9fa1c112be19dea5c2053
haribhatta <harishbnv@gmail.com>
2022-02-17 15:39:29 +0545

    updated login step to check Core-CFG-Parameter value

--- SystemTest/Billing/IPbilling/TC002IPbillingCalculationCredit.py
+++ SystemTest/Billing/IPbilling/TC002IPbillingCalculationCredit.py
@@ -8,34 +8,44 @@ import Library.ApplicationConfiguration as AC
 import Library.LibModuleAppointment as LA
 import Library.LibModuleBilling as LB
 import Library.LibModuleADT as LADT
-# front desk user login
+import Library.LibModuleSettings as LS
+# admin user login
 foUserId = GSV.foUserID
 foUserPwd = GSV.foUserPwD
+# front desk user login
+adminUserId = GSV.adminUserID
+adminUserPwd = GSV.adminUserPwD
 ########
 priceCategoryType = "Normal"
 discountScheme = GSV.discountSchemeName
 ########
 EMR = AC.openBrowser()
+AC.login(adminUserId, adminUserPwd)
+isDoctorMandatory = LS.checkCoreCFGparameter(danpheEMR=EMR)
+AC.logout()
 AC.login(foUserId, foUserPwd)
 LB.counteractivation(EMR)
 LADT.dischargeRandomPatient(EMR)  # This action is to free bed to admit new patient (Pre-condition to run test script).
 paymode = "CREDIT"
 HospitalNo = LA.patientquickentry(danpheEMR=EMR, discountScheme=0, paymentmode=paymode, department=GSV.departmentGyno, doctor=GSV.doctorGyno, priceCategoryType=priceCategoryType).HospitalNo
-LADT.admitDisTrans(EMR, 1, 0, 0, HospitalNo, 0, GSV.doctorGyno, GSV.departmentGyno)
+LADT.admitDisTrans(danpheEMR=EMR, admit=1, discharge=0, trasfer=0, HospitalNo=HospitalNo, deposit=0, doctor=GSV.doctorGyno, department=GSV.departmentGyno, admittingDoctorMandatory=isDoctorMandatory)
 LB.getIPbillingDetails(EMR, HospitalNo, paymode)
 LB.preIPbillingDetails(paymentmode="CREDIT")
 test1 = GSV.USG
 test1rate = GSV.usgRate
+### Create IP provisional bill
 LB.createIPprovisionalBill(EMR, HospitalNo, test1)
 LB.getIPbillingDetails(EMR, HospitalNo, paymode)
 LB.verifyIPbillingDetails(testrate=test1rate, canceltest=0, paymentmode=paymode)
 LB.preIPbillingDetails(paymentmode="CREDIT")
 test2 = GSV.BTCT
 test2rate = GSV.btctRate
+### Create IP provisional bill
 LB.createIPprovisionalBill(EMR, HospitalNo, test2)
 LB.getIPbillingDetails(EMR, HospitalNo, paymode)
 LB.verifyIPbillingDetails(testrate=test2rate, canceltest=0, paymentmode=paymode)
 LB.preIPbillingDetails(paymentmode="CREDIT")
+### Cancel IP provisional bill
 LB.cancelIPprovisionalBill(EMR, HospitalNo, test2)
 LB.getIPbillingDetails(danpheEMR=EMR, HospitalNo=HospitalNo, paymentmode=paymode)
 LB.verifyIPbillingDetails(testrate=0, canceltest=test2rate, paymentmode=paymode)
