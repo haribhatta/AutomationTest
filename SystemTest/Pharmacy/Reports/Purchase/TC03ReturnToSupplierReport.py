3c7bd7f45a96d2d053fb91867236e84342d80db6
DELL <puskarkhadka12@gmail.com>
2022-02-23 16:51:47 +0545

    small change in TC03ReturnToSupplierReport.py

--- SystemTest/Pharmacy/Reports/Purchase/TC03ReturnToSupplierReport.py
+++ SystemTest/Pharmacy/Reports/Purchase/TC03ReturnToSupplierReport.py
@@ -1,10 +1,6 @@
 import Library.GlobalShareVariables as GSV
 import Library.ApplicationConfiguration as AC
-import Library.LibModuleAppointment as LA
 import Library.LibModulePharmacy as LP
-import Library.LibModuleDispensary as LD
-import Library.LibModulePharmacyReports as LPR
-import Library.LibModuleBilling as LB
 import Library.LibModulePharmacyReports as LMPR
 
 
@@ -12,9 +8,6 @@ import Library.LibModulePharmacyReports as LMPR
 pharmacyUserId = GSV.pharmacyUserID
 pharmacyUserPwd = GSV.pharmacyUserPwD
 pharmacyUserName = GSV.pharmacyUserName
-# front desk user login
-billingId = GSV.foUserID
-billingPwd = GSV.foUserPwD
 drugname = GSV.drug1BrandName
 qty = 1
 rate = GSV.drug1Rate
