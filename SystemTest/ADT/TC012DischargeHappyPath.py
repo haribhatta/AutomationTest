62daef8af994a3e00638105df0ecbbbb47277460
haribhatta <harishbnv@gmail.com>
2022-02-27 10:27:05 +0545

    coreCFG parameter check TA is added

--- SystemTest/ADT/TC012DischargeHappyPath.py
+++ SystemTest/ADT/TC012DischargeHappyPath.py
@@ -7,7 +7,10 @@ import Library.ApplicationConfiguration as AC
 
 import Library.LibModuleBilling as LB
 import Library.LibModuleADT as ADT
-
+import Library.LibModuleSettings as LS
+# admin user login
+adminUserId = GSV.adminUserID
+adminUserPwd = GSV.adminUserPwD
 # front desk user login
 foUserId = GSV.foUserID
 foUserPwd = GSV.foUserPwD
@@ -21,10 +24,13 @@ deposit = 0
 #Scripted on: 12.05.2077
 
 EMR = AC.openBrowser()
-AC.login(foUserId, foUserPwd)
+AC.login(adminUserId, adminUserPwd)
+isDoctorMandatory = LS.checkCoreCFGadmitDocMandatory(danpheEMR=EMR)
 LB.counteractivation(EMR)
 for x in range(6):
-    ADT.admitDisTrans(danpheEMR=EMR, admit=0, discharge=1, trasfer=0, HospitalNo='Auto Test', deposit=0, doctor=0, department=0)
+    ADT.admitDisTrans(danpheEMR=EMR, admit=0, discharge=1, trasfer=0, deposit=0, HospitalNo='Auto Test',
+                      department=GSV.departmentGyno, doctor=GSV.doctorGyno, admittingDoctorMandatory=isDoctorMandatory)
+
 AC.logout()
 AC.closeBrowser()
 print("TC012 DischargeHappyPath: Passed")
