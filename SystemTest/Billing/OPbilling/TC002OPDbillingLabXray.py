b24f3bc4b33b240e2e7c5e7bc546f3fb10717ce1
DELL <puskarkhadka12@gmail.com>
2022-03-07 17:10:00 +0545

    Add hospitalnumber, invoice number and discount percent in patient quick entry

--- SystemTest/Billing/OPbilling/TC002OPDbillingLabXray.py
+++ SystemTest/Billing/OPbilling/TC002OPDbillingLabXray.py
@@ -19,7 +19,7 @@ doctorGynae = GSV.doctorGyno
 AC.login(foUserId, foUserPwd)
 LB.counteractivation(EMR)
 # 1. Create an appointment for new patient.
-HospitalNo = LA.patientquickentry(EMR, discountScheme=0, paymentmode='Cash', department=departmentGynae, doctor=doctorGynae, priceCategoryType="Normal").HospitalNo
+HospitalNo, InvoiceNo, discountPercentage = LA.patientquickentry(EMR, discountScheme=0, paymentmode='Cash', department=departmentGynae, doctor=doctorGynae, priceCategoryType="Normal")
 #can.verifyopdinvoice(deposit=0, billamt=500)
 print("Status:Passed - > TC001 CreateAppointmentNew")
 #oblx.verifyopdinvoice(deposit=0, billamt=500)
