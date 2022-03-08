035478160e798cd0f648677fc175268103c3ec73
DELL <puskarkhadka12@gmail.com>
2022-03-08 15:43:51 +0545

    Add invoice number and discount percentage in patientquickentry in TC005IncomeSegregationReport.py

--- SystemTest/Reports/TC005IncomeSegregationReport.py
+++ SystemTest/Reports/TC005IncomeSegregationReport.py
@@ -40,7 +40,7 @@ LB.counteractivation(EMR)
 #####Scenario: Cash Invoice with no Discount
 LBR.getIncomeSegregation(EMR)
 LBR.preSystemIncomeSegregation()
-InvoiceNo = LA.patientquickentry(danpheEMR=EMR, discountScheme=0, paymentmode='Cash', department=GSV.departmentGyno, doctor=GSV.doctorGyno, priceCategoryType=priceCategoryType).InvoiceNo
+HospitalNo, InvoiceNo, discountPercentage = LA.patientquickentry(danpheEMR=EMR, discountScheme=0, paymentmode='Cash', department=GSV.departmentGyno, doctor=GSV.doctorGyno, priceCategoryType=priceCategoryType)
 LBR.getIncomeSegregation(EMR)
 LBR.verifyIncomeSegregation(cash=rateOPD, cashReturn=0, credit=0, creditReturn=0, discount=0, provision=0)
 #####Scenario: Return Cash Invoice with no Discount
@@ -50,7 +50,7 @@ LB.returnBillingInvoice(danpheEMR=EMR, InvoiceNo=InvoiceNo, returnmsg="this is b
 LBR.getIncomeSegregation(EMR)
 LBR.verifyIncomeSegregation(cash=0, cashReturn=rateOPD, credit=0, creditReturn=0, discount=0, provision=0)
 #####Scenario: Credit Invoice with no Discount
-InvoiceNo1 = LA.patientquickentry(danpheEMR=EMR, discountScheme=0, paymentmode='Credit', department=GSV.departmentGyno, doctor=GSV.doctorGyno, priceCategoryType=priceCategoryType).InvoiceNo
+HospitalNo, InvoiceNo1, discountPercentage = LA.patientquickentry(danpheEMR=EMR, discountScheme=0, paymentmode='Credit', department=GSV.departmentGyno, doctor=GSV.doctorGyno, priceCategoryType=priceCategoryType)
 LBR.preSystemIncomeSegregation()
 LBR.getIncomeSegregation(EMR)
 LBR.verifyIncomeSegregation(cash=0, cashReturn=0, credit=rateOPD, creditReturn=0, discount=0, provision=0)
@@ -60,14 +60,13 @@ LB.returnBillingInvoice(danpheEMR=EMR, InvoiceNo=InvoiceNo1, returnmsg="this is 
 LBR.getIncomeSegregation(EMR)
 LBR.verifyIncomeSegregation(cash=0, cashReturn=0, credit=0, creditReturn=rateOPD, discount=0, provision=0)
 #####Scenario: Cash Invoice with Discount
-HospitalNo = LA.patientquickentry(danpheEMR=EMR, discountScheme=discountScheme, paymentmode='Cash', department=GSV.departmentGyno, doctor=GSV.doctorGyno, priceCategoryType=priceCategoryType).HospitalNo
-LADT.admitDisTrans(danpheEMR=EMR, admit=1, trasfer=0, discharge=0,deposit=0, HospitalNo=HospitalNo, doctor=doctor, department=department, admittingDoctorMandatory=isDoctorMandatory)
+HospitalNo, InvoiceNo, discountPercentage = LA.patientquickentry(danpheEMR=EMR, discountScheme=discountScheme, paymentmode='Cash', department=GSV.departmentGyno, doctor=GSV.doctorGyno, priceCategoryType=priceCategoryType)
+LADT.admitDisTrans(danpheEMR=EMR, admit=1, trasfer=0, discharge=0, deposit=0, HospitalNo=HospitalNo, doctor=doctor, department=department, admittingDoctorMandatory=isDoctorMandatory)
 LBR.getIncomeSegregation(EMR)
-LB.createIPprovisionalBill(danpheEMR=EMR,HospitalNo=HospitalNo, test=usgtest)
+LB.createIPprovisionalBill(danpheEMR=EMR, HospitalNo=HospitalNo, test=usgtest)
 LBR.preSystemIncomeSegregation()
 LBR.getIncomeSegregation(EMR)
 LBR.verifyIncomeSegregation(cash=0, cashReturn=0, credit=0, creditReturn=0, discount=0, provision=usgprice)
-
 AC.logout()
 AC.closeBrowser()
 
