e523fcab2488dc4f0bb78b6b83485f8b17ad91f8
DELL <puskarkhadka12@gmail.com>
2022-03-14 12:59:29 +0545

    change in TC03ReturnToSupplierReport.py add brandname and generic name

--- SystemTest/Pharmacy/Reports/Purchase/TC03ReturnToSupplierReport.py
+++ SystemTest/Pharmacy/Reports/Purchase/TC03ReturnToSupplierReport.py
@@ -11,13 +11,13 @@ pharmacyUserName = GSV.pharmacyUserName
 drugname = GSV.drug1BrandName
 qty = 1
 rate = GSV.drug1Rate
-supplier = GSV.supplier
+supplier = GSV.pharmacySupplierName1
 ########
 EMR = AC.openBrowser()
 AC.login(pharmacyUserId, pharmacyUserPwd)
 goodsReceiptNo = LP.createPharmacyGoodsReceipt(EMR, supplier=supplier, qty=2, DrugName=drugname, grPrice=5)
 print(goodsReceiptNo)
-LP.verifyPharmacyGoodsReceipt(EMR, DrugName=GSV.drug1BrandName, grno=goodsReceiptNo)
+LP.verifyPharmacyGoodsReceipt(EMR, brandName=drugname, genericName=GSV.drug1GenericName, grno=goodsReceiptNo)
 creditnote = LP.return_to_supplier(danpheEMR=EMR, grno=goodsReceiptNo, rqty=1)
 print(creditnote)
 LMPR.getReturnToSupplierReport(EMR, creditno=creditnote)
