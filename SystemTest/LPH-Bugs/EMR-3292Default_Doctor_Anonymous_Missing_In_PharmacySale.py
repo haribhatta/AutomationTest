209b844869c7e1d621e39894461d013f4af8da20
DELL <puskarkhadka12@gmail.com>
2022-04-05 11:22:31 +0545

    added EMR-3292Default_Doctor_Anonymous_Missing_In_PharmacySale.py

---
+++ SystemTest/LPH-Bugs/EMR-3292Default_Doctor_Anonymous_Missing_In_PharmacySale.py
@@ -0,0 +1,17 @@
+import Library.GlobalShareVariables as GSV
+import Library.ApplicationConfiguration as AC
+import Library.LibModuleDispensary as LD
+
+EMR = AC.openBrowser()
+#############
+## pharmacy user login
+phuserid = GSV.pharmacyUserID
+phuserpwd = GSV.pharmacyUserPwD
+drugSinex = GSV.drugSinexName
+
+paymentMode = 'Credit'
+AC.login(phuserid, phuserpwd)
+LD.activateDispensaryCounter(danpheEMR=EMR, dispensaryName=GSV.dispensaryName1)
+LD.createDispensarySaleRegisterOutdoorPatient(danpheEMR=EMR, HospitalNo=1, qty=1, drugName=GSV.drug1BrandName, paymentmode=paymentMode)
+AC.logout()
+AC.closeBrowser()
