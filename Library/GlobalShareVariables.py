2dbfe1d5c6263bf03cf3d621872262ec5a3fce5a
DELL <puskarkhadka12@gmail.com>
2022-04-11 17:31:09 +0545

    added EMR-5019_Billing_Invoice_ReferredBYDoctorNameIsMissingInBillingInvoice.py and change in LibModuleBilling.py and added ReferredBY in GlobalShareVariables.py

--- Library/GlobalShareVariables.py
+++ Library/GlobalShareVariables.py
@@ -171,7 +171,6 @@ if appName == "SNCH":
       admitRate = 1500
       discountCommunityName = "Social Service Unit"
       discountSchemeName = "Senior Citizen (10%)"
-      ReferredBy = "Dr. Anju Chaudhary"
 ###TestAction>>Pharmacy/Store+DispensaryItems:
       drug1BrandName = "SINEX TAB"
       drug1GenericName = "PARACETAMOL"
