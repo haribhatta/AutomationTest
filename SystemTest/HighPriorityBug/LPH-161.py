6e22c79be145ca8bc9c68c07bff23c2652c2f335
haribhatta <imarkgit@123>
2022-06-03 12:02:18 +0545

    updated as per Jira description.

--- SystemTest/HighPriorityBug/LPH-161.py
+++ SystemTest/HighPriorityBug/LPH-161.py
@@ -1,16 +1,35 @@
-from TestActionLibrary import A
-from TestActionLibrary import GSV
+'''Scripted by: Hari P. Bhatta
+Headline: Test_MMH-Pharmacy: Unable to request drug. Drop down box of drug list is not displayed.
+Description:
+Issue: No option to select drug to sale items from Pharmacy sale window.
 
-# front desk user login
-adminuser = GSV.adminUserID
-adminpswd = GSV.adminUserPwD
+Steps:
 
+go to pharmacy sale window. (/Home/Index#/Pharmacy/Sale/New)
 
+Select drug name in “Drug/Medicine Name” field.
 
-addEmp= A()
-addEmp.openBrowser()
-addEmp.login(adminuser, adminpswd)
-addEmp.Setting_add_employee()
-addEmp.logout()
-addEmp.login(adminuser, adminpswd)
-addEmp.Setting_Adding_User()
+Expected result: field to select drug name must be available.
+
+Actual result: field to select drug name is not available.
+'''
+import Library.GlobalShareVariables as GSV
+import Library.ApplicationConfiguration as AC
+import Library.LibModuleDispensary as LD
+
+EMR = AC.openBrowser()
+#############
+# pharmacy user login
+phuserid = GSV.pharmacyUserID
+phuserpwd = GSV.pharmacyUserPwD
+departmentGynae = GSV.departmentGyno
+doctorGynae = GSV.doctorGyno
+drugSinex = GSV.drugSinexName
+
+paymentMode = 'Cash'
+#############
+AC.login(phuserid, phuserpwd)
+LD.activateDispensaryCounter(danpheEMR=EMR, dispensaryName=GSV.dispensaryName1)
+LD.createDispensarySaleRandomPatient(danpheEMR=EMR, qty=1, drugname=drugSinex, paymentmode=paymentMode)
+AC.logout()
+AC.closeBrowser()
