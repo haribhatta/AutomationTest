8a3d1d3bf24e8cf02f5cd7f63f94846a1417dae5
Alina-Shrestha-Imark <100552399+Alina-Shrestha-Imark@users.noreply.github.com>
2022-11-10 15:06:56 +0545

    Fixed EMR-5321_ADT_DischargedPatient_WithoutFillingMandatoryFieldSummaryIsAdded.py

--- Library/LibModuleADT.py
+++ Library/LibModuleADT.py
@@ -175,6 +175,22 @@ def checkAutoAddItems(danpheEMR):
     # assert autoaddbillitemvalue == "autoaddbillitemvalue   False"      rework needed
     # assert autoaddBeditemvalue == "autoaddBeditemvalue   True"        rework needed
 
+def AddSummaryOfDischargedPatient(danpheEMR, HospitalNo, doctorName):
+    print("START>>AddingSummary")
+    danpheEMR.find_element(By.LINK_TEXT, "ADT").click()
+    time.sleep(3)
+    danpheEMR.find_element(By.LINK_TEXT, "Discharged Patients").click()
+    time.sleep(2)
+    danpheEMR.find_element(By.ID, "quickFilterInput").send_keys(HospitalNo)
+    time.sleep(3)
+    danpheEMR.find_element(By.LINK_TEXT, "Add Summary").click()
+    time.sleep(3)
+    DischargeType = Select(danpheEMR.find_element(By.XPATH, "/html/body/my-app/div/div/div[3]/div[2]/div/div/ng-component/ng-component/div/div[2]/discharge-summary-add/div/form/div/div[2]/div/div[2]/div[1]/div[1]/div[1]/div/select"))
+    DischargeType.select_by_visible_text("Referred")
+    time.sleep(3)
+    danpheEMR.find_element(By.XPATH, "//input[@placeholder='Consultant name']").send_keys(doctorName)
+    time.sleep(3)
+    danpheEMR.find_element(By.XPATH,"/html/body/my-app/div/div/div[3]/div[2]/div/div/ng-component/ng-component/div/div[2]/discharge-summary-add/div/form/div/div[2]/div/div[2]/div[2]/input").click()
 
 def wait_for_window(danpheEMR, timeout=2):
     time.sleep(round(timeout / 1000))
