dcafa8282b2683ab2580a23e039c4ef2e8e972f8
DELL <puskarkhadka12@gmail.com>
2022-11-25 11:17:09 +0545

    changed in function call in TC003returnIPcashBilling.py and in TC004IPbillingCreditSettlements.py

--- SystemTest/Billing/IPbilling/TC004IPbillingCreditSettlements.py
+++ SystemTest/Billing/IPbilling/TC004IPbillingCreditSettlements.py
@@ -17,12 +17,13 @@ adminUserPwd = GSV.adminUserPwD
 itemprice = GSV.admitRate
 doctor = GSV.doctorGyno
 department = GSV.departmentGyno
+priceCategoryType = "Normal"
 ########
 EMR = AC.openBrowser()
 AC.login(adminUserId, adminUserPwd)
 isDoctorMandatory = LS.checkCoreCFGadmitDocMandatory(danpheEMR=EMR)
 LB.counteractivation(EMR)
-HospitalNo = LPP.patientRegistration(EMR)
+HospitalNo, InvoiceNo, discountPercentage = LA.patientquickentry(danpheEMR=EMR, discountScheme=0, paymentmode="Cash", department=GSV.departmentGyno, doctor=GSV.doctorGyno, priceCategoryType=priceCategoryType, case='+ve')
 LADT.admitDisTrans(danpheEMR=EMR, admit=1, discharge=0, transfer=0, HospitalNo=HospitalNo, deposit=0, doctor=GSV.doctorGyno, department=GSV.departmentGyno, admittingDoctorMandatory=isDoctorMandatory)
 LB.generateDischargeInvoice(danpheEMR=EMR, creditOrganization=GSV.creditOrganization, HospitalNo=HospitalNo, paymentmode='CREDIT')
 LB.creditSettlements(danpheEMR=EMR, creditOrganization=GSV.creditOrganization, HospitalNo=HospitalNo, ProvisionalSlip='No', cashdiscount=0)
